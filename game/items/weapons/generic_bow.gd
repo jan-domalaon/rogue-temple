cd0bbc5cc12e9e0c112bf2a196097f768442c3f8
Jan D <jandomalaon@gmail.com>
2018-10-22 20:40:35 -0400

    Added projectile collision to walls, players, mobs. Bow targeting now working as intended (so far).

--- game/items/weapons/generic_bow.gd
+++ game/items/weapons/generic_bow.gd
@@ -6,16 +6,20 @@
 extends "res://game/items/weapons/weapon.gd"
 
 # Speed of the projectile shot
-export (int) var projectile_speed = 80
+export (int) onready var projectile_speed = 200
+export var projectile = "res://game/items/weapons/generic_projectile.tscn"
+export var piercing = false
 
 var can_fire = false
-
+var arrow = load(projectile)
 
 func make_draw_bow():
 	self.show()
 	$".".look_at(get_global_mouse_position())
 	$draw_timer.set_wait_time(primary_as)
 	# Disable weapon_area. The bow shouldn't damage if it is being drawn
+	
+	# Slow user's movespeed by a bit
 
 
 func make_reset_bow():
@@ -39,3 +43,12 @@ func fire_bow():
 		$weapon_area/sprite.set_region_rect(Rect2(0, 0, 16, 16))
 		$weapon_area/hitbox.set_disabled(true)
 		$".".hide()
+		
+		# Instance arrow, shoot towards mouse
+		var arrow_instance = arrow.instance()
+		# Add arrow to the world (level)
+		get_parent().get_parent().add_child(arrow_instance)
+		# Arrow spawns on the player's position
+		arrow_instance.position = get_parent().get_global_position()
+		# Arrow will go towards the mouse position (based on player's position, not global pos)
+		arrow_instance.set_vars(get_parent().get_local_mouse_position(), projectile_speed, user_type, primary_damage, primary_dmg_type, piercing)
--- game/items/weapons/generic_projectile.gd
+++ game/items/weapons/generic_projectile.gd
@@ -2,12 +2,48 @@
 
 # Script for projectiles (arrows, bolts).
 
-extends KinematicBody2D
+extends Area2D
+
+var proj_dmg
+var proj_dmg_type
+var proj_rot
+var proj_speed
+var proj_pierce
+var velocity
+var weapon_user
 
 
-func shot(position, speed):
-	# Movement direction is towards position
+func _ready():
+	#self.set_position(Vector2(0, 32))
 	pass
+
+
+func _physics_process(delta):
+	$".".show()
+	self.set_position(get_position() + velocity  * delta)
+
+
+func set_vars(dest, speed, user, dmg, dmg_type, is_piercing):
+	proj_speed = speed
+	velocity = speed * dest.normalized()
+	weapon_user = user
+	proj_dmg = dmg
+	proj_dmg_type = dmg_type
+	proj_pierce = is_piercing
+	look_at(get_global_mouse_position())
+
 
 func _on_projectile_body_entered(body):
-	pass
+	if (body.is_in_group("walls") or body.is_in_group("doors")):
+		queue_free()
+	if (weapon_user == "player"):
+		if (body.is_in_group("enemies")):
+			body.receive_phys_damage(proj_dmg, proj_dmg_type)
+			# If the proj is not "piercing", destroy on contact
+			if (not proj_pierce):
+				queue_free()
+	elif (weapon_user == "mob"):
+		if (body.is_in_group("player")):
+			body.receive_phys_damage(proj_dmg, proj_dmg_type)
+			if (not proj_pierce):
+				queue_free()
