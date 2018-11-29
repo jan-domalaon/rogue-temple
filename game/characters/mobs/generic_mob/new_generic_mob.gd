d3dd447ca8343e2c92d546eb19fa9e3dd529d142
NullBackspace <jandomalaon@gmail.com>
2018-11-28 19:15:22 -0500

    Work on new generic mob AI. Work on pathfinding and raycasting.

--- game/characters/mobs/generic_mob/new_generic_mob.gd
+++ game/characters/mobs/generic_mob/new_generic_mob.gd
@@ -26,6 +26,7 @@ var nav = null setget set_nav
 var path = [] 
 onready var nav_map = get_parent().get_node("nav")
 var old_player_pos
+var move_dir_chase = false
 
 # Attacking logic
 var can_attack = true
@@ -46,29 +47,26 @@ func _ready():
 func _physics_process(delta):
 	movement()
 	# Use move dir if player can be seen
-	if (detection_ray().collider.is_in_group("player")):
+	if ():
 		var current_player_pos = get_parent().get_node("player").get_global_position()
 		movement_dir = (current_player_pos - get_global_position()).normalized()
+		move_dir_chase = true
 	# Else use pathfinding. Go to each node of the path
 	else:
-		if path.size() > 1:
-			var dist = path[0] - get_global_position()
-			if dist.length() > 2:
-				# Move to next path node
-				movement_dir = (path[0] - get_global_position()).normalized()
-			else:
-				path.remove(0)
+		if (move_dir_chase):
+			# Get a new path by forcing pathfinding timer to time out
+			set_new_path()
+			move_dir_chase = false
 		else:
-			print("no more path nodes")
-			movement_dir = Vector2(0,0)
-	update()
-
-
-func _draw():
-	if path.size() > 1:
-		for node in path:
-			draw_circle(node - position, 5, Color(0, 1, 1))
-	draw_line(Vector2(), get_parent().get_node("player").position - get_global_position(), Color(1, 0, 0))
+			pathfinding()
+#	update()
+#
+#
+#func _draw():
+#	if path.size() > 1:
+#		for node in path:
+#			draw_circle(node - position, 5, Color(0, 1, 1))
+#	draw_line(Vector2(), get_parent().get_node("player").position - get_global_position(), Color(1, 0, 0))
 
 
 func set_nav(new_nav):
@@ -76,16 +74,46 @@ func set_nav(new_nav):
 
 
 func _on_pathfinding_timer_timeout():
+	set_new_path()
+
+
+func set_new_path():
+	set_nav(nav_map)
 	player_pos = get_parent().get_node("player").get_global_position()
 	path = nav.get_simple_path(get_global_position(), player_pos, false)
 	$pathfinding_timer.start()
 
 
-func detection_ray():
+func pathfinding():
+	if path.size() > 1:
+		var dist = path[0] - get_global_position()
+		if dist.length() > 2:
+			# Move to next path node
+			movement_dir = (path[0] - get_global_position()).normalized()
+		else:
+			path.remove(0)
+	else:
+		print("no more path nodes")
+		movement_dir = Vector2(0,0)
+
+
+func detection():
 	var current_player_pos = get_parent().get_node("player").get_global_position()
 	var physics_space = get_world_2d().direct_space_state
 	var ignore_areas = [self, $knockback_area]
+	var player_extents = get_parent().get_node("player/hitbox").shape.extents - Vector2(3, 3)
 	
+	var nw = current_player_pos - player_extents
+	var se = current_player_pos + player_extents
+	var ne = current_player_pos + Vector2(player_extents.x, -player_extents.y)
+	var sw = current_player_pos + Vector2(-player_extents.x, player_extents.y)
+	
+	for pos in [nw, ne, se, sw]:
+		detect_ray(pos, current_player_pos)
+
+
+func detect_ray(target_position, player_pos):
 	# Ignore mob mask (00101 == 5). Bit mask is in binary. Collides with players and walls only.
-	var detect_ray = physics_space.intersect_ray(get_global_position(), current_player_pos, ignore_areas, 5)
-	return detect_ray
+	var detect_ray = physics_space.intersect_ray(target_position, player_pos, ignore_areas, 5)
+	if (detect_ray.collider.is_in_group("player")):
+		return
