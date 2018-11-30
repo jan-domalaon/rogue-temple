cc06adf795d3faf354f6b18f9169168233451e27
NullBackspace <jandomalaon@gmail.com>
2018-11-30 15:25:36 -0500

    First iteration of new pathfinding for mobs.

--- game/characters/mobs/generic_mob/new_generic_mob.gd
+++ game/characters/mobs/generic_mob/new_generic_mob.gd
@@ -48,13 +48,20 @@ func _physics_process(delta):
 	movement()
 	var current_player_pos = get_parent().get_node("player").get_global_position()
 	var player_extents = get_parent().get_node("player/hitbox").shape.extents - Vector2(3, 3)
+	var mob_extents = $hitbox.shape.extents - Vector2(1, 1)
 	
 	var nw = current_player_pos - player_extents
 	var se = current_player_pos + player_extents
 	var ne = current_player_pos + Vector2(player_extents.x, -player_extents.y)
 	var sw = current_player_pos + Vector2(-player_extents.x, player_extents.y)
+	
+	var mob_nw = get_global_position() - mob_extents
+	var mob_se = get_global_position() + mob_extents
+	var mob_ne = get_global_position() + Vector2(mob_extents.x, -mob_extents.y)
+	var mob_sw = get_global_position() + Vector2(-mob_extents.x, mob_extents.y)
+	
 	# Use move dir if player can be seen by all corners
-	if (detect_ray(get_global_position(), nw) and detect_ray(get_global_position(), se) and detect_ray(get_global_position(), ne) and detect_ray(get_global_position(), sw)):
+	if (detect_ray(mob_nw, nw) and detect_ray(mob_se, se) and detect_ray(mob_ne, ne) and detect_ray(mob_sw, sw)):
 		movement_dir = (current_player_pos - get_global_position()).normalized()
 		move_dir_chase = true
 	# Else use pathfinding. Go to each node of the path
@@ -66,25 +73,31 @@ func _physics_process(delta):
 			move_dir_chase = false
 		else:
 			pathfinding()
-	update()
-
-
-func _draw():
-	if path.size() > 1:
-		for node in path:
-			draw_circle(node - position, 5, Color(0, 1, 1))
-	var current_player_pos = get_parent().get_node("player").get_global_position()
-	var player_extents = get_parent().get_node("player/hitbox").shape.extents - Vector2(3, 3)
-	
-	var nw = current_player_pos - player_extents
-	var se = current_player_pos + player_extents
-	var ne = current_player_pos + Vector2(player_extents.x, -player_extents.y)
-	var sw = current_player_pos + Vector2(-player_extents.x, player_extents.y)
-	
-	draw_line(Vector2(), nw - get_global_position(), Color(1, 0, 0))
-	draw_line(Vector2(), se - get_global_position(), Color(1, 0, 0))
-	draw_line(Vector2(), ne - get_global_position(), Color(1, 0, 0))
-	draw_line(Vector2(), sw - get_global_position(), Color(1, 0, 0))
+#	update()
+#
+#
+#func _draw():
+#	if path.size() > 1:
+#		for node in path:
+#			draw_circle(node - position, 5, Color(0, 1, 1))
+#	var current_player_pos = get_parent().get_node("player").get_global_position()
+#	var player_extents = get_parent().get_node("player/hitbox").shape.extents - Vector2(3, 3)
+#	var mob_extents = $hitbox.shape.extents - Vector2(1, 1)
+#
+#	var nw = current_player_pos - player_extents
+#	var se = current_player_pos + player_extents
+#	var ne = current_player_pos + Vector2(player_extents.x, -player_extents.y)
+#	var sw = current_player_pos + Vector2(-player_extents.x, player_extents.y)
+#
+#	var mob_nw = get_global_position() - mob_extents
+#	var mob_se = get_global_position() + mob_extents
+#	var mob_ne = get_global_position() + Vector2(mob_extents.x, -mob_extents.y)
+#	var mob_sw = get_global_position() + Vector2(-mob_extents.x, mob_extents.y)
+#
+#	draw_line(Vector2(), nw - mob_nw, Color(1, 0, 0))
+#	draw_line(Vector2(), se - mob_se, Color(1, 0, 0))
+#	draw_line(Vector2(), ne - mob_ne, Color(1, 0, 0))
+#	draw_line(Vector2(), sw - mob_sw, Color(1, 0, 0))
 
 
 func set_nav(new_nav):
