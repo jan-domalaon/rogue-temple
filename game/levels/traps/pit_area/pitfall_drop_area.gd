565190716f788da43acd6d6f5ff57525bad8f6e0
NullBackspace <jandomalaon@gmail.com>
2019-01-11 16:04:20 -0500

    Work on character health reduction on pitfall drop.

--- game/levels/traps/pit_area/pitfall_drop_area.gd
+++ game/levels/traps/pit_area/pitfall_drop_area.gd
@@ -19,10 +19,20 @@ func _ready():
 			char_instance.position.x = fmod(randi(), drop_area_extents.x) - (drop_area_extents.x/2) + (center_pos.x)
 			char_instance.position.y = fmod(randi(), drop_area_extents.y) - (drop_area_extents.y/2) + (center_pos.y)
 			
+			# Cut HP to half. Shows they dropped into the pit.
+			char_instance.health = char_instance.health / 2
+			
 			# Add instance to scene
 			add_child(char_instance)
 		else:
-			# This node is the player
+			# This node is the player. Only move player to drop position as it is already in the level
 			get_parent().get_parent().get_node("player").position.x = fmod(randi(), drop_area_extents.x) - (drop_area_extents.x/2) + (center_pos.x)
 			get_parent().get_parent().get_node("player").position.y = fmod(randi(), drop_area_extents.y) - (drop_area_extents.y/2) + (center_pos.y)
+			
+			# Cut player HP to half. If it's at 1, keep at 1
+			if get_parent().get_parent().get_node("player").health > 1:
+				print("player health into pit. health > 1")
+				get_parent().get_parent().get_node("player").health = get_parent().get_parent().get_node("player").health / 2
+			else:
+				get_parent().get_parent().get_node("player").health = 1
 		# Level change is in charge of clearing pitfall global vars
