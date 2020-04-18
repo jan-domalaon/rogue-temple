babf46cf19367c52abdda0bfca7c6b5f6d56f358
NullBackspace <jandomalaon@gmail.com>
2020-04-18 18:45:03 -0400

    Created menu transitions for main menu. Created a simple screen swipe transition using a mask and shader.

--- menu/main_menu/menu.gd
+++ menu/main_menu/menu.gd
@@ -5,19 +5,17 @@ extends Control
 
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	$splash/AnimationPlayer.play("splash_anim")
+	# Connect to signal when animation is finished
+	# Play menu button animation
+	$AnimationPlayer.play("switch_screen_anim")
 	set_process_input(true)
 
 func _on_play_button_pressed():
-	# Change scene to save scene, where player can choose new game or load a saved game
-	get_tree().change_scene("res://menu/save_menu/save_screen.tscn")
+	switch_screens("switch_screen_anim", "res://menu/save_menu/save_screen.tscn")
 
 
 func _on_options_pressed():
-	switch_screen_anim()
-#	get_tree().change_scene("res://menu/options/options.tscn")
+	switch_screens("screen_swipe", "res://menu/options/options.tscn")
 
 
 func _on_quit_button_pressed():
@@ -25,5 +23,24 @@ func _on_quit_button_pressed():
 	get_tree().quit()
 
 
-func switch_screen_anim():
-	$CenterContainer/VBoxContainer/play_button.disabled = true
+func switch_screens(anim, scene_path):
+	# Disable menu buttons to prevent triggering other scene changes
+	disable_menu_buttons()
+	
+	# Play screen animation defined.
+	# If using switch_screen_anim, play backwards. 
+	# If using mask transition, turn on visibility
+	if (anim == "switch_screen_anim"):
+		$AnimationPlayer.play_backwards(anim)
+	elif (anim == "screen_swipe"):
+		$menu_transition.show()
+		$AnimationPlayer.play(anim)
+	# Once animation is done, change scenes
+	yield($AnimationPlayer, "animation_finished")
+	get_tree().change_scene(scene_path)
+
+
+func disable_menu_buttons():
+	# Disables all buttons in the menu
+	for button in $CenterContainer/menu_button_container.get_children():
+		button.set_disabled(true)
