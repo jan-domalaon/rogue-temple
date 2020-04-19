73de0cb006d50317e1fc6192de1bdeadd0e9207a
Jan D <jandomalaon@gmail.com>
2020-04-19 14:40:07 -0400

    Changed starting level to prototype_level. Fixed door functionality in test_level_0

--- menu/save_menu/save_screen.gd
+++ menu/save_menu/save_screen.gd
@@ -12,7 +12,7 @@ func _on_new_game_pressed():
 	# In the future, there might be a build_screen before a new_level
 	# For now, load level and clear previous save
 	save.clear_save()
-	get_tree().change_scene('res://game/levels/level_0/test_level.tscn')
+	get_tree().change_scene('res://game/levels/level_0/prototype_level.tscn')
 
 
 func _on_load_pressed():
