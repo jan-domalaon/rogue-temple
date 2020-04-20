9b1ec93428c7f034e06810b948505745f31ca688
Jan D <jandomalaon@gmail.com>
2020-04-20 15:40:41 -0400

    Disabled save button when there is no save present

--- menu/save_menu/save_screen.gd
+++ menu/save_menu/save_screen.gd
@@ -6,6 +6,11 @@ extends Container
 func _ready():
 	# Reset the continue_game flag in save singleton
 	save.continue_game = false
+	
+	# Toggle load menu only when a save is present. Clear tooltip
+	if save.save_file_exists():
+		$new_game_cont/load.set_disabled(false)
+		$new_game_cont/load.set_tooltip("")
 
 
 func _on_new_game_pressed():
