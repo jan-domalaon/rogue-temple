8cdd9d037eefd665d6c014e503421415c4eae5dd
Jan D <jandomalaon@gmail.com>
2020-04-20 15:07:48 -0400

    Prototype prep: added transition to Options screen. Initial implementation of Credits and Changelog buttons.

--- menu/options/options.gd
+++ menu/options/options.gd
@@ -5,10 +5,39 @@ extends Node
 var prev_scene = null
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process_input(true)
+	# Play intro transition (function calls are handled here also)
+	$animation_player.play("screen_swipe_open_left")
+
+
+func disable_menu_buttons(toggle):
+	# Disables all buttons that are tagged as options
+	for button in get_tree().get_nodes_in_group("option"):
+		if toggle:
+			button.set_disabled(true)
+		else:
+			button.set_disabled(false)
+
 
 func _on_back_pressed():
+	# Play close transition and head back to main menu
 	get_tree().change_scene("res://menu/main_menu/menu.tscn")
+
+
+func _on_changelog_pressed():
+	# Open the Updates page for game (For now, redirect to github repo)
+	OS.shell_open("https://github.com/jan-domalaon/rogue-temple")
+
+
+func _on_credits_pressed():
+	# Switch to credits menu (for now, redirects to README)
+	OS.shell_open("https://github.com/jan-domalaon/rogue-temple/blob/master/README.md")
+
+
+func _on_audio_pressed():
+	pass # Replace with function body.
+
+
+func _on_video_pressed():
+	pass # Replace with function body.
 
